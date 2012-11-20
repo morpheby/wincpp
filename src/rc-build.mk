47e4cdbe7c0ef19cdbbeb061fc972f437caca806
Ilya Mikhaltsou <morpheby@gmail.com>
2012-11-20 06:01:48 +0300

    Updated manifests to accommodate AMD64 architecture

--- src/rc-build.mk
+++ src/rc-build.mk
@@ -3,11 +3,12 @@
 SUFFIXES += .rc .manifest
 
 VERSION_4D  = @VERSION_4D@
+MANIFEST_ARCH = @MANIFEST_ARCH@
 
 .rc.$(OBJEXT):
 	$(RESCOMP) --use-temp-file -i $< $(INCLUDES) $(DEFAULT_INCLUDES) -I./resources $(DEFS) -o $@
 
 .premf.manifest:
-	$(SED) 's/_VERSION_/$(VERSION_4D)/g' $< > $@
+	$(SED) 's/_VERSION_/$(VERSION_4D)/g' $< | $(SED) 's/_ARCH_/$(MANIFEST_ARCH)/g' > $@
 
 
