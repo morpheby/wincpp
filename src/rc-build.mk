726a8a86cf09f8db909fc5cedd027f48ccea8594
Ilya Mikhaltsou <morpheby@gmail.com>
2013-01-05 19:59:54 +0300

    Switched resources to UTF-8

--- src/rc-build.mk
+++ src/rc-build.mk
@@ -6,7 +6,7 @@ VERSION_4D  = @VERSION_4D@
 MANIFEST_ARCH = @MANIFEST_ARCH@
 
 .rc.$(OBJEXT):
-	$(RESCOMP) --use-temp-file -i $< $(INCLUDES) $(DEFAULT_INCLUDES) -I./resources $(DEFS) -o $@
+	$(RESCOMP) --use-temp-file --codepage=65001 -i $< $(INCLUDES) $(DEFAULT_INCLUDES) -I./resources $(DEFS) -o $@
 
 .premf.manifest:
 	$(SED) 's/_VERSION_/$(VERSION_4D)/g' $< | $(SED) 's/_ARCH_/$(MANIFEST_ARCH)/g' > $@
